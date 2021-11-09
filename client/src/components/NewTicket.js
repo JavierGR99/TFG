c72ab6a9f637a62a85b6a5ab34ea8bd157d5c828
Javier_GR <javigallego99@gmail.com>
2021-11-09 13:34:25 +0100

    server updated

---
+++ client/src/components/NewTicket.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function NewTicket() {
+
+
+    async function getApartaments() {
+
+    }
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default NewTicket
